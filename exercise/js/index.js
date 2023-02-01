ae517cb48d6e0e201441361d1ecb951995477004
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-02-01 12:27:21 +0100

    change directory name

---
+++ exercise/js/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "Bootcamp Web Development";
+
+const text =
+  "Im currently taking part at Neue Fische Bootcamp to become a Web Developer. ";
+
+const likes = 20;
+
+const user = "Jana";
+
+const isReported = true;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(user);
+console.log(isReported);
+console.log(likes + 1);
+
+// --^-- write your code here --^--
